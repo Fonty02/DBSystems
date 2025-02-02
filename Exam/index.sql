e353154216b6e6cb10253d12a711b46be9c3eae7
Fontana <fontanaemanuele14@gmail.com>
2025-02-02 16:08:49 +0100

    Minor changes

--- Exam/index.sql
+++ Exam/index.sql
@@ -8,6 +8,39 @@ CREATE INDEX Employee_DoB_Index ON Employee(Dob);
 --INDEX TEST
 SET AUTOTRACE ON
 
+--Operation1
+INSERT INTO CUSTOMER
+VALUES (
+CustomerTY
+(
+    'CustomerTrace', 
+    'NameTrace', 
+    'SurnameTrace', 
+    'ciao@gmail.com',
+    'Commercial',
+    TO_DATE('1985-01-01','YYYY-MM-DD')
+)
+);
+
+--Operation2
+
+INSERT INTO Contract
+VALUES (
+ContractTY(
+    'ContractTestIndex', 
+    'Y', 
+    'Commercial', 
+    TO_DATE('2023-01-01','YYYY-MM-DD'), 
+    100, 
+    2500, 
+    1, 
+    (SELECT REF(a) FROM Account a WHERE a.Code = 'AccountTest'), 
+    (SELECT REF(f) FROM Facility f WHERE f.Name = 'FacilityTest3')
+)
+);
+
+
+
 --Operation 3
 
 SELECT COUNT(*) INTO v_count_facility
@@ -25,5 +58,11 @@ SELECT DEREF(e.Team).Code AS team_code
              ORDER BY e.DoB ASC
              FETCH FIRST 1 ROWS ONLY;
 
+
+
+--Operation 5
+SELECT Name, EfficiencyScore
+        FROM Facility
+       ORDER BY EfficiencyScore DESC;
 
 
